71e36d54f3df7478408d3500aa73270f3f1ccde2
Bruno Lopes <bruno.lopes@unifor.br>
2021-03-13 20:36:42 -0300

    Pequeno ajuste

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -6,7 +6,7 @@ fun main() {
 
 
     val props = Properties()
-    props.load(App::class.java.getResourceAsStream("/twitch-sample.properties"))
+    props.load(App::class.java.getResourceAsStream("/twitch.properties"))
     val channel = props.getProperty("BOT_TWITCH_USERNAME")
     println(channel)
 
